f9c3206a3481039129a6e649e6963c881e354e6b
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 15:12:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.length.times |index|
+    if array[index] == value_to_find 
+      return index 
+    end 
 end
 
 def find_max_value(array)
